04a73f0b23c328bf82c572cd57a75e646d122e89
Thomas Helland <thomas_helland@pm.me>
2025-04-02 13:30:46 +0200

    :)))))))

--- src/Todo.cpp
+++ src/Todo.cpp
@@ -3,15 +3,15 @@
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
-#include <iterator>
 #include <vector>
 
-Todo::Todo() {}
+Todo::Todo() {
+  todoList = {{"Bake brød", 10}, {"Spise brød", 100}, {"Gå tur", 1000}, {"Kjøre bil", 3}};
+}
 
 Todo::~Todo() {}
 
-void Todo::add_todo_item()
-{
+void Todo::add_todo_item() {
   std::string a;
   uint32_t b;
   std::cout << "### Add an item ###" << "\n\n\n" << "Write task name: ";
@@ -22,55 +22,63 @@ void Todo::add_todo_item()
   todoList.push_back({a, b});
 }
 
-void Todo::show_all_tasks()
-{
-  std::sort(todoList.begin(), todoList.end(), [](const TodoItems& a, const TodoItems& b) {
-    return a.importance < b.importance;
-  });
+void Todo::show_all_tasks() {
+  std::sort(todoList.begin(), todoList.end(),
+            [](const TodoItems &a, const TodoItems &b) {
+              return a.importance < b.importance;
+            });
 
-  for (auto it = todoList.begin(); it != todoList.end(); it++)
-  {
+  for (auto it = todoList.begin(); it != todoList.end(); it++) {
     std::cout << it->taskName << "\n" << it->importance << "\n";
   }
 }
 
-void Todo::show_winner()
-{
- auto mostImportant = std::max_element(todoList.begin(), todoList.end(),
-        [](const TodoItems& a, const TodoItems& b) {
-            return a.importance < b.importance;
-        });
+void Todo::show_winner() {
+  auto mostImportant =
+      std::max_element(todoList.begin(), todoList.end(),
+                       [](const TodoItems &a, const TodoItems &b) {
+                         return a.importance < b.importance;
+                       });
 
- if (mostImportant != todoList.end())
- {
-   std::cout << mostImportant->taskName << "\n" << mostImportant->importance;
- }
+  if (mostImportant != todoList.end()) {
+    std::cout << mostImportant->taskName << "\n" << mostImportant->importance;
+  }
 }
 
-void Todo::todo_game()
-{
+void Todo::todo_game() {
   std::vector<TodoItems> todos = todoList;
 
-  int z = -1;
-  
-  for (size_t i = 0; i < todos.size(); i++)
-  {
-    if (todos.size() <= 1)
-    {
-      std::cout << todos[i].taskName << " is the winner! :D" << "\n";
-      return;
-    }
-    std::cout << "[1]" << todos[i].taskName << "\n" << "[2]" << todos[i++].taskName << "\n";
-    std::cin >> z;
+  int z = -1, count = 0;
 
-    switch (z)
-    {
-    case 1:
-      todos.erase(todos.begin()+1);
-      break;
-    case 2:
-      todos.erase(todos.begin());
-      break;
+  while (todos.size() > 1) {
+    for (size_t i = 0; i < todos.size(); i++) {
+      count = i + 1;
+
+      if (i == todos.size() - 1)
+      {
+        count = i - 1;
+        // continue;
+      }
+
+      std::cout << "[1]" << todos[i].taskName << "\n"
+                << "[2]" << todos[count].taskName << "\n";
+      std::cin >> z;
+
+      switch (z) {
+      case 1:
+        todos.erase(todos.begin() + count);
+        for (size_t i = 0; i < todos.size(); i++) {
+          std::cout << todos[i].taskName << "\n" << todos[i].importance << "\n";
+        }
+        break;
+      case 2:
+        todos.erase(todos.begin() + i);
+        for (size_t i = 0; i < todos.size(); i++) {
+          std::cout << todos[i].taskName << "\n" << todos[i].importance << "\n";
+        }
+        break;
+      }
     }
   }
-}    
+  std::cout << todos[0].taskName << " is the winner! :D" << "\n";
+}
